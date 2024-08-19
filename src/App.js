8f1982a1f12f33e380fdf5366be96f9da8c7fae7
RyanNolan1 <114003965+RyanNolan1@users.noreply.github.com>
2024-08-19 12:43:32 +0100

    this will console log the api data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [cars, setCars] = useState([]);
+
+  fetch('https://corsproxy.io/?https://m6zhmj6dggvrmepfanilteq4q40rlalu.lambda-url.eu-west-1.on.aws/vehicles')
+  .then((res) => res.json())
+  .then((data) => console.log(data))
+
 }
 
 export default App;
+
+
