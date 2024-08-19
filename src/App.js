e61db68cad84293c6d8b6797a24688a9edc17336
RyanNolan1 <114003965+RyanNolan1@users.noreply.github.com>
2024-08-19 13:48:38 +0100

    this will wrap the fetch request in useEffect so that it is only called on the first load

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import './App.css';
-import { useState } from "react";
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
   const [cars, setCars] = useState([]);
 
-  fetch('https://corsproxy.io/?https://m6zhmj6dggvrmepfanilteq4q40rlalu.lambda-url.eu-west-1.on.aws/vehicles')
-  .then((res) => res.json())
-  .then((data) => console.log(data))
+  useEffect(function () {
+    fetch(
+      "https://corsproxy.io/?https://m6zhmj6dggvrmepfanilteq4q40rlalu.lambda-url.eu-west-1.on.aws/vehicles"
+    )
+      .then((res) => res.json())
+      .then((data) => setCars(data.data));
+  }, []);
 
+  console.log(cars)
+
+  return (
+    <div>
+      <ul></ul>
+    </div>
+  );
 }
 
 export default App;
-
-
