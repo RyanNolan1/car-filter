4d33299bf2afbfdf39fa1e852e6664f3975d6b3d
RyanNolan1 <114003965+RyanNolan1@users.noreply.github.com>
2024-08-22 09:06:14 +0100

    this will add the final styling to to the value form

--- src/App.js
+++ src/App.js
@@ -295,11 +295,11 @@ function ValueForm() {
       </div>
       <form className="value-form">
         <label class="form-label">
-          <p className="input-title">VRM</p>
-          <input className="form-input" type="text" name="VRM" placeholder="Enter VRM" />
+          <p className="input-title">VRM<span class="asterisk">*</span></p>
+          <input maxlength="10" className="form-input" type="text" name="VRM" placeholder="Enter VRM" />
         </label>
         <label class="form-label">
-          <p className="input-title">Mileage</p>
+          <p className="input-title">Mileage<span>*</span></p>
           <input className="form-input" type="number" name="Mileage" placeholder="Enter mileage" />
         </label>
         <input
